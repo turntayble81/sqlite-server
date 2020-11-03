f1105027b617cd3db40c618da5aaab6da98c7374
Haig Papaghanian <haig.papaghanian@gmail.com>
2020-11-03 01:13:12 -0500

    Fixed several issues

--- server.js
+++ server.js
@@ -3,20 +3,64 @@ const sqlite3 = require('sqlite3');
 const uuid    = require('uuid');
 const config  = require('./config');
 
+let reqStr = '';
+const selectRe = /^\s*select/;
+const queryRe = /^.*?\0(.*)/;
 const server = net.createServer();
+
 server.maxConnections = config.MAX_CONNECTIONS;
 server.listen(config.PORT, '127.0.0.1');
 
-function logConnectionCount() {
-    server.getConnections((err, count) => {
-        console.log(`Active connections: ${count}`);
-    });
-}
 
 /*** EVENTS ***/
 
 server.on('connection', (socket) => {
 
+    function closeConnection() {
+        socket.db.close();
+        socket.destroy();
+        logConnectionCount();
+    }
+
+    function logConnectionCount() {
+        server.getConnections((err, count) => {
+            console.log(`Active connections: ${count}`);
+        });
+    }
+    
+    function processRequestData() {
+        let matchResults = reqStr.match(queryRe);
+        while(Array.isArray(matchResults) && matchResults.length == 2) {
+            const startTime = Date.now();
+            let fn = 'all';
+            const req = matchResults[0];
+            reqStr = matchResults[1];
+
+            if(!selectRe.test(req.toLowerCase())) {
+                fn = 'exec';
+            }
+
+            //receiving req here
+            console.log(`Got request from connection ${socket.id}`);
+
+            //do something here
+            socket.db[fn](req, (err, data) => {
+                console.log(`Request complete for connection ${socket.id} in ${Date.now() - startTime}ms`);
+                if(err) {
+                    console.error(err);
+                    socket.write(JSON.stringify(err.toString({error: err.toString()})));
+                    return;
+                }
+                if(fn == 'all') {
+                    socket.write(JSON.stringify(data));
+                }else {
+                    socket.write('{}');
+                }
+            });
+            matchResults = reqStr.match(queryRe);
+        }
+    }
+    
     const db = new sqlite3.Database(config.DATABASE_FILE);
     socket.db = db;
     socket.id = uuid.v4();
@@ -32,39 +76,19 @@ server.on('connection', (socket) => {
 
     socket.on('data', (req) => {
         req = req.toString();
+        reqStr += req;
 
-        // TODO: Should only handle one request at a time per connection
-
-        // TODO: For select queries, use all or each.
-        // For all others, use exec:
-
-        // Database#all(sql, [param, ...], [callback])
-        // Database#each(sql, [param, ...], [callback], [complete])
-        // Database#exec(sql, [callback])
-
-        //receiving req here
-        console.log(`Got request from connection ${socket.id}: `, req);
-
-        //do something here
-        socket.db.all(req, (err, data) => {
-            if(err) {
-                socket.write(err);
-                return;
-            }
-            socket.write(JSON.stringify(data));
-        });
+        processRequestData();
     });
 
     socket.on('timeout', () => {
         console.log(`Connection timed out. Connection identifier: ${socket.id}`);
-        socket.destroy();
-        logConnectionCount();
+        closeConnection();
     });
 
     socket.on('end', (data) => {
         console.log(`Connection closed. Connection identifier: ${socket.id}`);
-        socket.destroy();
-        logConnectionCount();
+        closeConnection();
     });
 });
 
