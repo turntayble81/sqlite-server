a5735c5a010ccdf2ef12360c83af81c402927d33
Haig Papaghanian <haig.papaghanian@gmail.com>
2020-11-03 23:50:47 -0500

    Refactored server. Renamed client to cli script

--- server.js
+++ server.js
@@ -3,106 +3,118 @@ const sqlite3 = require('sqlite3');
 const uuid    = require('uuid');
 const config  = require('./config');
 
-let reqStr = '';
-const selectRe = /^\s*?select[^;]*?[\s;]*$/i;
-const dmlRe = /^\s*?(insert|update|replace|delete)[^;]*?[\s;]*$/i;
-const queryRe = /^.*?\0(.*)/;
-const nullByteRe = /\0$/;
-const server = net.createServer();
+class SqliteServer {
 
-server.maxConnections = config.MAX_CONNECTIONS;
-server.listen(config.PORT, '127.0.0.1');
+    // TODO: pass config vars to constructor
+    constructor() {
+        this.server = net.createServer();
+        this.reqStr = '';
 
 
-/*** EVENTS ***/
+        this.server.maxConnections = config.MAX_CONNECTIONS;
+        this.server.listen(config.PORT, '127.0.0.1');
 
-server.on('connection', (socket) => {
+        this.server.on('listening', () => {
+            console.log(`Server listening on port ${config.PORT}`);
+        });
 
-    function closeConnection() {
-        socket.db.close();
-        socket.destroy();
-        logConnectionCount();
+        this.server.on('connection', (socket) => {this.newConnection(socket)});
     }
 
-    function logConnectionCount() {
-        server.getConnections((err, count) => {
-            console.log(`Active connections: ${count}`);
+    newConnection(socket) {
+        this.socket = socket;
+
+        const db = new sqlite3.Database(config.DATABASE_FILE);
+        this.socket.db = db;
+        this.socket.id = uuid.v4();
+
+        console.log(`Connection created from ${this.socket.remoteAddress}. Connection identifier: ${this.socket.id}`);
+        this.logConnectionCount();
+
+        //Close connections which broke on the client side
+        this.socket.setKeepAlive(true, config.CONNECTION_TIMEOUT * 1000);
+
+        //Close idle connections
+        this.socket.setTimeout(config.CONNECTION_TIMEOUT * 1000);
+
+        this.socket.on('data', (req) => {
+            req = req.toString();
+            this.reqStr += req;
+
+            this.processRequestData();
+        });
+
+        this.socket.on('timeout', () => {
+            console.log(`Connection timed out. Connection identifier: ${this.socket.id}`);
+            this.closeConnection();
+        });
+
+        this.socket.on('end', (data) => {
+            console.log(`Connection closed. Connection identifier: ${this.socket.id}`);
+            this.closeConnection();
         });
     }
-    
-    function processRequestData() {
-        let matchResults = reqStr.match(queryRe);
+
+    closeConnection() {
+        this.socket.db.close();
+        this.socket.destroy();
+        this.logConnectionCount();
+    }
+
+    processRequestData() {
+        const _this = this;
+        let matchResults = this.reqStr.match(SqliteServer.queryRe);
+
         while(Array.isArray(matchResults) && matchResults.length == 2) {
             const startTime = Date.now();
             let fn;
             let req = matchResults[0].toLowerCase();
-            req = req.replace(nullByteRe, '');
+            req = req.replace(SqliteServer.nullByteRe, '');
 
-            reqStr = matchResults[1];
+            this.reqStr = matchResults[1];
 
             console.log(req);
-            if(selectRe.test(req)) {
+            if(SqliteServer.selectRe.test(req)) {
                 fn = 'all';
-            }else if(dmlRe.test(req)) {
+            }else if(SqliteServer.dmlRe.test(req)) {
                 fn = 'run';
             }else {
                 fn = 'exec';
             }
             console.log(fn);
             //receiving req here
-            console.log(`Got request from connection ${socket.id}`);
+            console.log(`Got request from connection ${this.socket.id}`);
 
             //do something here
-            socket.db[fn](req, function(err, data) {
-                console.log(`Request complete for connection ${socket.id} in ${Date.now() - startTime}ms`);
+            this.socket.db[fn](req, function(err, data) {
+                console.log(`Request complete for connection ${_this.socket.id} in ${Date.now() - startTime}ms`);
                 if(err) {
                     console.error(err);
-                    socket.write(JSON.stringify({error: err.toString()}));
+                    _this.socket.write(JSON.stringify({error: err.toString()}));
                     return;
                 }
                 if(fn == 'all') {
-                    socket.write(JSON.stringify(data));
+                    _this.socket.write(JSON.stringify(data));
                 }else if(fn == 'run') {
-                    socket.write(JSON.stringify({ lastId: this.lastID, changes: this.changes }));
+                    _this.socket.write(JSON.stringify({ lastId: this.lastID, changes: this.changes }));
                 }else {
-                    socket.write('{}');
+                    _this.socket.write('{}');
                 }
             });
-            matchResults = reqStr.match(queryRe);
+            matchResults = this.reqStr.match(SqliteServer.queryRe);
         }
     }
-    
-    const db = new sqlite3.Database(config.DATABASE_FILE);
-    socket.db = db;
-    socket.id = uuid.v4();
 
-    console.log(`Connection created from ${socket.remoteAddress}. Connection identifier: ${socket.id}`);
-    logConnectionCount();
+    logConnectionCount() {
+        this.server.getConnections((err, count) => {
+            console.log(`Active connections: ${count}`);
+        });
+    }
+}
 
-    //Close connections which broke on the client side
-    socket.setKeepAlive(true, config.CONNECTION_TIMEOUT * 1000);
+SqliteServer.selectRe = /^\s*?select[^;]*?[\s;]*$/i;
+SqliteServer.dmlRe = /^\s*?(insert|update|replace|delete)[^;]*?[\s;]*$/i;
+SqliteServer.queryRe = /^.*?\0(.*)/;
+SqliteServer.nullByteRe = /\0$/;
 
-    //Close idle connections
-    socket.setTimeout(config.CONNECTION_TIMEOUT * 1000);
-
-    socket.on('data', (req) => {
-        req = req.toString();
-        reqStr += req;
-
-        processRequestData();
-    });
-
-    socket.on('timeout', () => {
-        console.log(`Connection timed out. Connection identifier: ${socket.id}`);
-        closeConnection();
-    });
-
-    socket.on('end', (data) => {
-        console.log(`Connection closed. Connection identifier: ${socket.id}`);
-        closeConnection();
-    });
-});
-
-server.on('listening', () => {
-    console.log(`Server listening on port ${config.PORT}`);
-});
+new SqliteServer();
